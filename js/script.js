bd73332bb494c5d4320b5ac85514ad9a5390523a
Badsha Hossain <badshab454@gmail.com>
2023-10-30 22:56:41 +0600

    api name

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const apiKey = "1f3e0ad7fc8a1ffbc14d7ef4150d7b04";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=dhaka";
+
+async function checkWeather(){
+    const response = await fetch (apiUrl + `&appid=${apiKey}`);
+    var data = await response.json();
+    console.log(data);
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp)+'°C';
+    document.querySelector(".humidity").innerHTML = data.main.humidity+'%';
+    document.querySelector(".wind").innerHTML = data.wind.speed+'km/h';
+} 
+
+checkWeather();
+
+
